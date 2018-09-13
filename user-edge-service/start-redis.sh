0a2e3acb805505cb74e384fae3213544916d4758
mayanghua <yh_ma@qq.com>
2018-09-13 17:03:46 +0800

    tt

--- user-edge-service/start-redis.sh
+++ user-edge-service/start-redis.sh
@@ -2,4 +2,4 @@
 
 docker stop redis
 docker rm redis
-docker run --name user-redis -v /ops/data/redis/conf/redis.conf:/etc/redis/redis.conf -v /ops/data/redis/data:/data -p 6379:6379 -idt redis:3.2 redis-server /etc/redis/redis.conf --appendonly yes --restart always
+docker run --name user-redis -v /ops/data/redis/conf/redis.conf:/etc/redis/redis.conf -v /ops/data/redis/data:/data -p 6379:6379 -idt redis:3.2 redis-server /etc/redis/redis.conf --appendonly yes
--- user-thrift-service/start-mysql.sh
+++ user-thrift-service/start-mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop user-mysql
 docker rm user-mysql
-docker run --name user-mysql -v /ops/data/mysql/conf:/etc/mysql/conf.d -v /ops/data/mysql/data:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7  --restart always
+docker run --name user-mysql -v /ops/data/mysql/conf:/etc/mysql/conf.d -v /ops/data/mysql/data:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
