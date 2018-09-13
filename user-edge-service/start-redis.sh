48ea935c6fec17b504881d1a5590d5c82ad48a23
mayanghua <yh_ma@qq.com>
2018-09-13 14:42:01 +0800

    tt

--- user-edge-service/start-redis.sh
+++ user-edge-service/start-redis.sh
@@ -1,15 +1,5 @@
-#!/bin/bash
-cur_dir=pwd
+﻿#!/bin/bash
+
 docker stop redis
 docker rm redis
-docker run --name user-redis -v ${cur_dir}/conf/redis.conf:/etc/redis/redis_default.conf -v ${cur_dir}/data:/data -p 6379:6379 -idt redis:3.2
-docker run --name user-redis -v conf/redis.conf:/etc/redis/redis.conf -v data:/data -p 6379:6379 -idt redis:3.2 redis-server /etc/redis/redis.conf --appendonly yes
-#命令说明：
-　　--name redis3 : 指定容器名称，这个最好加上，不然在看docker进程的时候会很尴尬。
-　　-p 6699:6379 ： 指定端口映射，默认redis启动的是6379，至于外部端口不冲突就行。
-　　-v $PWD/redis.conf:/etc/redis/redis.conf ： 将主机中当前目录下的redis.conf配置文件映射。
-　　-v $PWD/data:/data -d redis:3.2 ： 将主机中当前目录下的data挂载到容器的/data
-　　--redis-server --appendonly yes :在容器执行redis-server启动命令，并打开redis持久化配置\
-　　注意事项：
-　　　　如果不需要指定配置，-v $PWD/redis.conf:/etc/redis/redis.conf 可以不用 ，
-　　　　redis-server 后面的那段 /etc/redis/redis.conf 也可以不用。
+docker run --name user-redis -v /ops/data/redis/conf/redis.conf:/etc/redis/redis.conf -v /ops/data/redis/data:/data -p 6379:6379 -idt redis:3.2 redis-server /etc/redis/redis.conf --appendonly yes --restart always
--- user-thrift-service/start-mysql.sh
+++ user-thrift-service/start-mysql.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cur_dir=pwd
 docker stop user-mysql
 docker rm user-mysql
-docker run --name user-mysql -v ${cur_dir}/conf:/etc/mysql/conf.d -v ${cur_dir}/data:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
-docker run --name user-mysql -v conf:/etc/mysql/conf.d -v data:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
+docker run --name user-mysql -v /ops/data/mysql/conf:/etc/mysql/conf.d -v /ops/data/mysql/data:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7  --restart always
